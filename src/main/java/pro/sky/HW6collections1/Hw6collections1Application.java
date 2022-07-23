83880996f7ebae71b6b504545f3f1fc4aa3406bc
MaraOD555 <ruffbc@hotmail.com>
2022-07-23 22:03:13 +0300

    Master commit

---
+++ src/main/java/pro/sky/HW6collections1/Hw6collections1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.HW6collections1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw6collections1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw6collections1Application.class, args);
+	}
+
+}
